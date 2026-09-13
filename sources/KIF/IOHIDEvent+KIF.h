e6105be8e767eb87f91e834097860928528a18cd
r1noz <r1ttoz@inbox.ru>
2026-09-13 22:10:26 +0700

    Update IOHIDEvent+KIF.h

--- sources/KIF/IOHIDEvent+KIF.h
+++ sources/KIF/IOHIDEvent+KIF.h
@@ -6,7 +6,7 @@
 //
 
 #import <Foundation/Foundation.h>
-#import "IOKit+SPI.h"
+#import "../../headers/IOKit+SPI.h"
 
 typedef struct __IOHIDEvent * IOHIDEventRef;
 IOHIDEventRef kif_IOHIDEventWithTouches(NSArray *touches) CF_RETURNS_RETAINED;
